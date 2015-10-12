15c2be48052323c01e1fbbc838d144c88552c720
Jianing Yang <jianingy.yang@gmail.com>
2015-10-12 14:45:53 +0800

    initial commit

---
+++ miniond/miniond.hs
@@ -0,0 +1,90 @@
+module Main where
+
+import Data.Text as T
+import qualified Text.ParserCombinators.Parsec as P
+import Text.Parsec ((<|>))
+import System.IO
+import System.Process
+
+
+data ModuleName = ModuleName String deriving (Show)
+data ModuleArgument = ModuleArgument String deriving (Show)
+data Command = Command { cmdModule :: ModuleName
+                       , cmdArgs :: [ModuleArgument]
+                       } deriving (Show)
+
+
+ident :: P.Parser String
+ident = P.many (P.oneOf "-_" <|> P.letter <|> P.digit)
+
+symbol :: P.Parser Char
+symbol = P.oneOf "!$%&|*+-/:<=>?@^_~"
+
+escapes :: P.Parser Char
+escapes = P.char '\\' >> P.oneOf "\\\"rnt" >>= \x -> return $ case x of
+    'r' -> '\r'
+    'n' -> '\n'
+    't' -> '\t'
+    _ -> x
+
+argumentToString :: ModuleArgument -> String
+argumentToString (ModuleArgument x) = x
+
+parseModuleName :: P.Parser ModuleName
+parseModuleName = ident >>= \x -> return $ ModuleName x
+
+parseQuotedString :: P.Parser ModuleArgument
+parseQuotedString = do
+  _ <- P.char '"'
+  s <- P.many1 $ escapes <|> P.noneOf "\""
+  _ <- P.char '"'
+  return $ ModuleArgument s
+
+parseString :: P.Parser ModuleArgument
+parseString = do
+  s <- P.many1 (P.letter <|> P.digit <|> symbol)
+  return $ ModuleArgument s
+
+parseCommand :: P.Parser Command
+parseCommand = do
+  name <- parseModuleName
+  args <- P.option [] (P.char ' ' >> P.sepEndBy (parseQuotedString <|> parseString) P.spaces)
+
+  return $ Command name args
+
+
+runModule :: Command -> Either String (IO String)
+runModule Command {cmdModule=(ModuleName "dmesg")} =
+  Right $ readProcess "dmesg" [] []
+
+runModule Command {cmdModule=(ModuleName "uptime")} =
+  Right $ readProcess "uptime" [] []
+
+runModule Command {cmdModule=(ModuleName "md5sum"), cmdArgs=args} =
+  Right $ readProcess "md5sum" [filepath] []
+  where filepath=argumentToString $ args !! 0
+
+runModule Command {cmdModule=(ModuleName "sha1sum"), cmdArgs=args} =
+  Right $ readProcess "sha1sum" [filepath] []
+  where filepath=argumentToString $ args !! 0
+
+runModule Command {cmdModule=(ModuleName "pidof"), cmdArgs=args} =
+  Right $ readProcess "pidof" [process] []
+  where process=argumentToString $ args !! 0
+
+runModule Command {cmdModule=(ModuleName "tail"), cmdArgs=args} =
+  Right $ readProcess "tail" [filepath] []
+  where filepath=argumentToString $ args !! 0
+
+runModule _ = Left "ERROR: Invalid module name"
+
+main :: IO ()
+main = do
+  input <- hGetLine stdin
+
+  case P.parse parseCommand "(command)" input of
+    Left _ -> putStrLn $ "ERROR: syntax error"
+    Right val -> case runModule val of
+                   Left err -> putStrLn err
+                   Right out -> do { x <- out;
+                                     putStrLn . T.unpack . T.strip . T.pack $ x }
